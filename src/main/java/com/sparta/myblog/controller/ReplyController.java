a1fc0c8052de7a924ef596e752e5f0d58e81beb8
TaeKyeong <murphytklee@gmail.com>
2023-04-26 02:06:19 +0900

    Feat : 댓글 수정, 삭제 기능 추가

--- src/main/java/com/sparta/myblog/controller/ReplyController.java
+++ src/main/java/com/sparta/myblog/controller/ReplyController.java
@@ -1,9 +1,11 @@
 package com.sparta.myblog.controller;
 
+import com.sparta.myblog.dto.MessageDto;
 import com.sparta.myblog.dto.ReplyReponseDto;
 import com.sparta.myblog.dto.ReplyRequestDto;
 import com.sparta.myblog.service.ReplyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,8 +23,14 @@ public class ReplyController {
         return replyService.createReply(postId, requestDto, request);
     }
     //댓글 수정
-
+    @PutMapping("/{replyId}")
+    public ReplyReponseDto updateReply(@PathVariable Long replyId, @RequestBody ReplyRequestDto requestDto, HttpServletRequest request){
+        return replyService.updateReply(replyId, requestDto, request);
+    }
     //댓글 삭제
-
+    @DeleteMapping("/{replyId}")
+    public ResponseEntity<MessageDto> deleteReply(@PathVariable Long replyId, HttpServletRequest request){
+        return replyService.deleteReply(replyId, request);
+    }
 
 }
--- src/main/java/com/sparta/myblog/service/ReplyService.java
+++ src/main/java/com/sparta/myblog/service/ReplyService.java
@@ -1,19 +1,21 @@
 package com.sparta.myblog.service;
 
+import com.sparta.myblog.dto.MessageDto;
 import com.sparta.myblog.dto.ReplyReponseDto;
 import com.sparta.myblog.dto.ReplyRequestDto;
-import com.sparta.myblog.entity.Post;
-import com.sparta.myblog.entity.Reply;
-import com.sparta.myblog.entity.Users;
+import com.sparta.myblog.entity.*;
 import com.sparta.myblog.jwt.JwtUtil;
 import com.sparta.myblog.repository.PostRepository;
 import com.sparta.myblog.repository.ReplyRepository;
 import com.sparta.myblog.repository.UserRepository;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.print.attribute.standard.MediaSize;
 import javax.servlet.http.HttpServletRequest;
 import java.util.NoSuchElementException;
 @Service
@@ -36,8 +38,30 @@ public class ReplyService {
     }
 
     //댓글 수정
-
+    public ReplyReponseDto updateReply(Long replyId, ReplyRequestDto requestDto, HttpServletRequest request){
+        String token = getToken(request);
+        Claims claims = jwtUtil.getUserInfoFromToken(token);
+        Users user = findUser(claims);
+        Reply reply = findReplyById(replyId);
+        if (user.getRole().equals(UserRoleEnum.USER)){
+            isUserReply(user,reply);
+        }
+        reply.update(requestDto, user);
+        return new ReplyReponseDto(reply);
+    }
     //댓글 삭제
+    public ResponseEntity<MessageDto> deleteReply(Long replyId, HttpServletRequest request){
+        String token = getToken(request);
+        Claims claims = jwtUtil.getUserInfoFromToken(token);
+        Users user = findUser(claims);
+        Reply reply = findReplyById(replyId);
+        if (user.getRole().equals(UserRoleEnum.USER)){
+            isUserReply(user,reply);
+        }
+        replyRepository.delete(reply);
+        MessageDto messageDto = MessageDto.setSuccess(StatusEnum.OK.getStatusCode(), "댓글 삭제 완료", null);
+        return new ResponseEntity(messageDto, HttpStatus.OK);
+    }
 
 
     //----
@@ -62,5 +86,15 @@ public class ReplyService {
         }
         return token;
     }
-
+    //댓글 확인
+    public Reply findReplyById(Long id){
+        return replyRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("댓글이 존재하지 않습니다."));
+    }
+    //댓글 주인 확인
+    public void isUserReply(Users user, Reply reply){
+        if (!reply.getUser().getId().equals(user.getId())) {
+            throw new IllegalArgumentException("작성자만 수정, 삭제 가능합니다.");
+        }
+    }
 }
